fa5ee4b618c828c3525b76e97f5c795f1c1da429
Prawee Wongsa <prawee.w@integra8t.com>
2019-03-14 16:37:29 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,18 @@
+// import library
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            //component about react native
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        );
+    }
+}
+
+// export 
+export default App;
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
 import {name as appName} from './app.json';
+import App from './components/App';
 
 AppRegistry.registerComponent(appName, () => App);
