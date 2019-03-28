6709e06423cc50feeb085274380819a2e6a4fdfa
Computer Education <computereducation@Macmini-213-Teacher.local>
2019-03-28 15:21:46 +0700

    create CardSection component

---
+++ components/CardSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CardSection extends Component {
+    render() {
+        return (
+            <View>
+                <Text>XXXX</Text>
+            </View>
+        );
+    }
+}
+
+export default CardSection;
