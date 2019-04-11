fe3e7a7ba935eb29f4ba60d44cb44c5fb9882352
Prawee Wongsa <prawee.w@integra8t.com>
2019-04-11 15:46:17 +0700

    add navigation

--- android/app/src/main/java/com/std4820750141/MainActivity.java
+++ android/app/src/main/java/com/std4820750141/MainActivity.java
@@ -1,6 +1,9 @@
 package com.std4820750141;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +15,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "std4820750141";
     }
+
+    @Override
+   protected ReactActivityDelegate createReactActivityDelegate() {
+     return new ReactActivityDelegate(this, getMainComponentName()) {
+       @Override
+       protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+       }
+     };
+   }
 }
--- index.js
+++ index.js
@@ -3,9 +3,19 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 // import App from './App';
 import {name as appName} from './app.json';
-import App from './components/App';
+// import App from './components/App';
 import About from './components/About';
+import Login from './components/Login';
 
-AppRegistry.registerComponent(appName, () => About);
+///
+
+const MainNavigator = createStackNavigator({
+    Login: {screen: Login},
+    Profile: {screen: About},
+});
+const App = createAppContainer(MainNavigator);
+
+AppRegistry.registerComponent(appName, () => App);
